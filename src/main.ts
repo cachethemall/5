3adbeee92824ff99c44ad294d54efeec8f83b4f4
CodeSandbox <support@codesandbox.io>
2023-10-07 07:12:53 +0000

    Initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,21 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+//import 'https://unpkg.com/bootstrap/dist/css/bootstrap.min.css'
+// import 'https://unpkg.com/bootstrap/dist/js/bootstrap.bundle.min.js'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import { exampleApexCharts2 } from "./exampleCode";
+import { mainMenu } from "./mainMenu";
+import { showChart } from "./chartLib";
+import { render, h } from "preact";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+async function StartUp() {
+  let dataJson = await fetch("./data.json").then((response) => response.text());
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  showChart(dataJson);
+  console.log("abc1");
+}
+
+// StartUp();
+// exampleApexCharts();
+
+// @ts-ignore
+render(h(mainMenu, null), document.getElementById("appContainer")!);
